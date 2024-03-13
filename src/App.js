5ac07ab53a13ba33b8adeedcde2a6e19268c5017
MaybeRavenn <mayberavenn@macbook-pro.myfiosgateway.com>
2024-03-12 20:51:45 -0400

    tweet

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React App with Netlify Function</h1>
+        <h1>Blahhhhhn</h1>
       </header>
       <main>
         <webhookTest /> {/* Use your webhook component */}
