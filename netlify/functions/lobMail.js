7497239c7f14e5070d8b0aa04a28fa21766dae5d
MoonGoop <45396753+MoonGoop@users.noreply.github.com>
2024-04-09 15:50:13 -0400

    time

--- netlify/functions/lobMail.js
+++ netlify/functions/lobMail.js
@@ -6,25 +6,45 @@ exports.handler = async function (event) {
 
     const body = JSON.parse(event.body);
 
-    // Log the entire body to see what's received
     console.log("Custom Data:", body.customData);
     console.log("Full Body:", body);
+
+    // Set a timeout for the Axios request
+    const axiosConfig = {
+      timeout: 5000, // timeout after 5 seconds
+    };
 
     const response = await axios.post(
       "https://mailer-vert-delta.vercel.app/api/scraper",
       {
         MailData: body.customData,
-      }
+      },
+      axiosConfig
     );
 
     console.log("Response from API:", response.data);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Data posted successfully" }),
+      body: JSON.stringify({
+        message: "Data posted successfully",
+        data: response.data,
+      }),
     };
   } catch (error) {
     console.error("Error in process:", error);
+
+    // Handle specific timeout error
+    if (error.code === "ECONNABORTED") {
+      console.error("Request timed out:", error.message);
+      return {
+        statusCode: 408, // Request Timeout
+        body: JSON.stringify({
+          message: "Request timed out",
+          error: error.message,
+        }),
+      };
+    }
 
     return {
       statusCode: 500,
